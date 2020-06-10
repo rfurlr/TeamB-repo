dfcb1e15cf270f86f4fd07fb9c7bfc83eb9e8d10
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-06-10 16:04:54 -0500

    Create LightSensor.c

---
+++ LightSensor.c
@@ -0,0 +1,47 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/cores.h>
+#include <stdbool.h>
+
+//initializes the warning diode
+void warning_init(){
+	
+	DDRB |= _BV(DDB);
+	
+}
+
+//initializes the all clear diode
+void allclear_init(){
+	
+	DDRB |= _BV(DDB);
+	
+}
+
+
+//turns on the warning diode
+void Light_warning_on(){
+	
+	PORTB |= _BV(PORTB);
+	
+}
+
+//turns off warning diode
+void Light_warning_off(){
+	
+	PORTB &= ~_BV(PORTB);
+}
+
+//turns on all clear diode
+void Light_allclear_on(){
+	
+	PORTB |= _BV(PORTB);
+	
+}
+
+
+//turns off all clear diode
+void Light_allclear_off(){
+			
+	PORTB &= ~_BV(PORTB);
+	
+}
