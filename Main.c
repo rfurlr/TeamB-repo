5823b8865eb5b65256be164313fae6723a5d9e87
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-06-10 09:41:56 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,24 @@
+/******************  Arduino Baremetal C Cabin Control Systems  ******************************/
+/*****************                  Authors                      *****************************/
+/****************                                                 ****************************/
+/***************                                                   ***************************/
+/**************	                                                    **************************/
+/*************	                                                     *************************/
+/************	                                                        ************************/ 
+	
 #include <stdbool.h>
 #include <avr/io.h> 
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 
+void Main(){
+	int y = 0;
+	//run the process as an infinite loop that starts when the machine starts, and stops when the machine is off
+	while (y < 2){
+			
+		
+	}
+
+	
+	
+}
