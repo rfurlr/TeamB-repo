f26cca872e67511b235d09115ddea811b9bec5bf
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-06-10 16:25:25 -0500

    Update processes.h

--- processes.h
+++ processes.h
@@ -2,3 +2,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void Light_warning_init();
+void Light_allclear_init();
+void Light_warning_on();
+void Light_warning_off();
+void Light_allclear_on();
+void Light_allclear_off();
