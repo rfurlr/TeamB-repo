b20384b8b35a38a99216583408bb054447ef1d0a
Erick-D-S <65356418+Erick-D-S@users.noreply.github.com>
2020-06-11 16:22:10 -0500

    added 1 second timer on interrupt

--- Main.c
+++ Main.c
@@ -13,10 +13,23 @@
 #include <util/delay.h>
 #include "processes.h"
 #include <stdint.h>       // needed for uint8_t
+#include <avr/interrupt.h>
+
+int seconds = 0; //counts seconds through timer and interrupt
 
 void Main(){
 	int y = 0;
 	int temp = 0;
+	
+	OCR1A = 0x3D08;
+	TCCR1B |= (1 << WGM12); // Mode 4, CTC on OCR1A
+    	
+	TIMSK1 |= (1 << OCIE1A);  //Set interrupt on compare match
+   
+	TCCR1B |= (1 << CS12) | (1 << CS10); // set prescaler to 1024 and start the timer
+    
+	sei(); // enable interrupts
+    
 	
 	//run the process as an infinite loop that starts when the machine starts, and stops when the machine is off
 	while (!y){
@@ -61,21 +74,26 @@ int tempSensor()
 
 int ADCsingleREAD(uint8_t adctouse){
 	
-    int ADCval;
+    	int ADCval;
 
-    ADMUX = adctouse;         // use inputted ADC
-    ADMUX |= (1 << REFS0);    // use AVcc as the reference
-    ADMUX &= ~(1 << ADLAR);   // clear for 10 bit resolution
+    	ADMUX = adctouse;         // use inputted ADC
+    	ADMUX |= (1 << REFS0);    // use AVcc as the reference
+    	ADMUX &= ~(1 << ADLAR);   // clear for 10 bit resolution
     
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);    // 128 prescale for 8Mhz
-    ADCSRA |= (1 << ADEN);    // Enable the ADC
+    	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);    // 128 prescale for 8Mhz
+    	ADCSRA |= (1 << ADEN);    // Enable the ADC
 
-    ADCSRA |= (1 << ADSC);    // Start the ADC conversion
+    	ADCSRA |= (1 << ADSC);    // Start the ADC conversion
 
-    while(ADCSRA & (1 << ADSC));      // This line waits for the ADC to finish 
+    	while(ADCSRA & (1 << ADSC));      // This line waits for the ADC to finish 
 
-    ADCval = ADCL;
-    ADCval = (ADCH << 8) + ADCval;    // ADCH is read so ADC can be updated again
+    	ADCval = ADCL;
+    	ADCval = (ADCH << 8) + ADCval;    // ADCH is read so ADC can be updated again
 
-    return ADCval;
+    	return ADCval;
+}
+	
+ISR (TIMER1_COMPA_vect)
+{
+  	seconds++; //every second this interrupt will go off and increment seconds.
 }
